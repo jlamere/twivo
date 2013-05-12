fe33e8e41e47d10bb8a3e453207c5e1f74020887
Jen Lamere <jennielamere@gmail.com>
2013-05-12 19:47:49 -0400

    clean

--- twivo.user.js
+++ twivo.user.js
@@ -1,140 +1,139 @@
 // ==UserScript==
-// @name        Twitter: Filter vine
+// @name        Twitter: Twivo App
 // @namespace   com.jennielamere.userscript.filtervine
 // @include     https://twitter.com/*
 // @include     http://twitter.com/*
 // @version     1.0.0
 // ==/UserScript==
-function main(){
+
+
+if (window.top != window.self)  //-- Don't run on frames or iframes.
+    return;
+
+function scriptMain () {
 
 var hide = new Array();
-var lockBox = new Array();
-<<<<<<< HEAD
-//var timeout = null;
-var kills = new Array ();
-=======
 var timeout = null;
 var kills = new Array ();
-var count = 0;
 var button;
->>>>>>> addArray
-if (!String.prototype.contains) {
-	String.prototype.contains = function(str, ignoreCase) {
-        return (ignoreCase ? this.toUpperCase() : this).indexOf(ignoreCase ? str.toUpperCase() : str) >= 0;
-	};
-}
-function pushArray(){
+var celeb = new Array();
 
-<<<<<<< HEAD
-	hide.push("fan");
-	hide.push("super");
-=======
-	hide.push("birthday");
-	hide.push("answer");
->>>>>>> addArray
+if (!String.prototype.contains) {
+    String.prototype.contains = function(str, ignoreCase) {
+        return (ignoreCase ? this.toUpperCase() : this).indexOf(ignoreCase ? str.toUpperCase() : str) >= 0;
+    };
 }
 function filtertweets() {
-	var tweets = document.getElementsByClassName("tweet");
+    var tweets = document.getElementsByClassName("tweet");
+    for (var i = 0; i < tweets.length; i++) {
+         var tweetContainer = tweets[i];
+         var tweet = tweets[i].innerHTML;
+         for (var j = 0; j < hide.length; j++){
+            if(tweetContainer.twivoHide == true){
+                continue;
+            }
+             if (tweet.contains(hide[j])) {
+                 tweetContainer.twivoHide = true;
+                 var parent = $(tweets[i]).parent();
+                 var user = $(tweets[i]).attr("data-user-id"); 
+                  kills.push(parent.clone());
+                 
+                 $(tweets[i]).css("background-color", "#787274");
+                
+                 $(tweets[i]).css("color", "#787274");
+             }
+         }
+     }  
+ }
 
-	for (var i = 0; i < tweets.length; i++) {
-        tweet = tweets[i].innerHTML;
-        for (var j = 0; j < hide.length; j++){
-      	  if (tweet.contains(hide[j]) && !(tweets.hidden)) {
-      	  	 tweets[i].hidden = true;
-<<<<<<< HEAD
-      	  //	 tweets[i].timestamp = new Date().getTime();
-=======
-      	  	 var timeRecorded = new Date().getTime();
-
-      	  	 tweets[i].timestamp = timeRecorded - new Date().getTime();
->>>>>>> addArray
-      	  	 lockBox.push(tweets[i]);
-			 kills.push(tweets[i]);
-       		 }
-   	 }
-    }
-    console.log(kills);
-    
-	for (var i = 0; i < kills.length; i++) { 
-		kills[i].style.display = "none";
-	}
-}
 function recordMode(){
-	if(timeout == null){
-	 timeout = setTimeout(filtertweets, 100);
-	 }
-}	
-function stopRecordMode(){
-	clearTimeout(timeout);
-	timeout = null;
-}
-<<<<<<< HEAD
+    if(timeout == null){
 
-addArray();
-=======
+     timeout = setInterval(filtertweets, 500);
+     }
+}   
+function stopRecordMode(){
+    clearTimeout(timeout);
+    timeout = null;
+}
 function addTwivoButton() {
-    var actions = jQ("#global-actions");
-    var li = jQ("<li class='new'>");
-    button = jQ("<button id='record' class='btn btn-primary'>");
+    var actions = $("#global-actions");
+    var input = $("<input type='text' id='tag'>");
+    var litext = $("<li class = 'new'>");
+    var li = $("<li class='new'>");
+    button = $("<button id='record' class='btn btn-primary'>");
     button.text('Record');
     li.append(button);
+    litext.append(input);
     actions.append(li);
-    jQ('#record').live('click', clicked);
-}
+    actions.append(litext);
+    button.click(clicked);
+} 
+addTwivoButton
 function clicked(){
-	console.log("yay");
-	/*
-	if (button.text == 'Record'){
-		recordMode();
-		button.text = ('Stop');
+    if (button.text() == 'Record'){
+         hide = [];
+         var tag = $("#tag").val();
+         var splitter = tag.split(",");
+        for(var i = 0; i < splitter.length; i++){
+           hide.push(splitter[i]);
+            }
+        for(var i = 0; i < splitter.length; i++){
+           hide.push(splitter[i].toLowerCase());
+            }
+        for(var i = 0; i < splitter.length; i++){
+           
+            hide.push(splitter[i].replace(/ /g,""));
+            }
+         button.text('Stop');
+         recordMode();
+    }
+    else if (button.text() == 'Stop'){
+         button.text('Play');
+         stopRecordMode();
+    }
+    else if (button.text() == 'Play'){
+         button.text('Record');
+         hide = [];
+         play();
+         kills = [];
+    }    
+} 
 
-	}
-	if (button.text == 'Stop'){
-		stopRecordMode();
-		button.text = ('Play');
-	}
-	if (button.text = 'Play'){
-		button.text = ('Record');
-	}
+ function play(){
+     var ol = $("#stream-items-id");
+     var count = 1;
 
-
-	*/
+     for (var i = kills.length-1; i >=0 ; i--) { 
+        
+         killIt(kills[i], ol, count*1000); 
+         count += 2;
+    }
 }
-/*
-	function play(){
-	var startTime = new Date().getTime();
-	var i = 0;
-	while(i < lockBox.length){
-		
-		var currentTime = new Date().getTime();
-		var timeIsGood = currentTime - currentTime;
-		if (timeIsGood == lockBox[i].timestamp){
-			console.log("tweet tweet");
-			i++;
-			}
-		}
-	}
+function killIt(tweet, list, delay){     
+     setTimeout(function(){
+         tweet.css('background-color', '#FAD860');
+          list.prepend(tweet);
+      },delay);
 
->>>>>>> addArray
+     }
+     addTwivoButton();    
+ }
+window.addEventListener ("load", scriptMainLoader, false);
 
-
-*/
-
- 	addTwivoButton();
- 	pushArray();
-	recordMode();
-
+function scriptMainLoader () {
+   addJS_Node (null, null, scriptMain);
 }
+function addJS_Node (text, s_URL, funcToRun) {
+    var D  = document;
+    var scriptNode = D.createElement
+('script');
+    scriptNode.type                         = "text/javascript";
+    if (text)       scriptNode.textContent  = text;
+    if (s_URL)      scriptNode.src          = s_URL;
+    if (funcToRun)  scriptNode.textContent  = '(' + funcToRun.toString() + ')()';
 
-function addJQuery(callback) {
-  var script = document.createElement("script");
-  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
-    document.body.appendChild(script);
-  }, false);
-  document.body.appendChild(script);
+    var targ = D.getElementsByTagName ('head')[0] || D.body ||
+D.documentElement;
+    targ.appendChild (scriptNode);
 }
-addJQuery(main);
-
